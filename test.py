42bb0d8395bef3e7b73f4a99afbb9de8780fd3f6
panchalnihar <154651425+PanchalNihar@users.noreply.github.com>
2025-07-21 13:12:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 print("Hello, World!\n");
-print("This is a simple Python script that prints "Hello, World!" to the console.")
+print("This is a simple Python script that prints Hello, World! to the console.")
